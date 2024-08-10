3721b628ccba701b3d0e3dd97cb465fc613057d6
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-08-10 17:43:48 +0300

    Completed the React hooks useEffect lab.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 1);
+      }
+    }, 1000);
+    //returning a cleanup function
+    return function cleanup() {
+      clearTimeout(timerID);
+    };
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  });
 
   const { id, prompt, answers, correctIndex } = question;
 
